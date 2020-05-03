9df27b4760bfee17dddb9c6b31b1c58b9c7f7f6e
Amirtaraj Ragavan <amragavan@BLR.ALLEGISINDIA.COM>
2020-05-03 18:46:40 +0530

    build1

---
+++ build/precache-manifest.a5157b040858bf2ca98e537089c3f6e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e926e8041de1dcc2bf35ca5a59f31bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3006412c9efd3f6ff88",
+    "url": "/static/css/main.89de0e51.chunk.css"
+  },
+  {
+    "revision": "c9536376adf358d398fe",
+    "url": "/static/js/2.92f2d1a7.chunk.js"
+  },
+  {
+    "revision": "e3006412c9efd3f6ff88",
+    "url": "/static/js/main.e5807cee.chunk.js"
+  },
+  {
+    "revision": "0f41967b12bfa929374d",
+    "url": "/static/js/runtime~main.e8ad4fbd.js"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/static/media/header-bg.e2d2a51c.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/static/media/map-image.f40ed5b1.png"
+  }
+]);
